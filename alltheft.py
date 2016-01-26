129bf1d522d59ed30c5a113e329f95b1c7d0226a
Rikk Hill <rikk@unashamedly.co.uk>
2016-01-26 12:47:58 +0000

    Started perceptron, some work on performance utilities

--- alltheft.py
+++ alltheft.py
@@ -2,13 +2,21 @@
 import pandas as pd
 import numpy as np
 import helper.data as hd
+import helper.performance as perf
 import helper.plot as plot
 import helper.constants as C
 
-code = C.CODE['LARCENY/THEFT']
+# Get a training split
+(train, test) = hd.get_training_split()
 
-vec = np.zeros((C.TEST_ROWS,), dtype = np.int)
+# Make a vector where every code is LARCENY/THEFT
+code = C.CODE['LARCENY/THEFT']
+vec = np.zeros((len(test),), dtype = np.int)
 vec.fill(code)
 
+# What's the out-of-sample error?
+oos = perf.oos_error(vec, test)
+print(oos)
 
-hd.submit(vec, 'alltheft')
+
+#hd.submit(vec, 'alltheft')
